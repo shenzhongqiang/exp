757d053dfe4f4dea448908a3f772321ef3a86044
Zhongqiang Shen <zqshen.1985@gmail.com>
2015-09-15 10:14:17 +0800

    test case for report

--- test/main/java/report/ReportTest.java
+++ test/main/java/report/ReportTest.java
@@ -15,15 +15,6 @@ public class ReportTest {
     @Before
     public void setUp() {
 		this.session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = session.beginTransaction();
-		// create default account with balance $5000
-		this.account = new Account(5000);
-		session.save(account);
-		tx.commit();
-    }
-
-    @After
-    public void tearDown() {
 		Transaction tx = this.session.beginTransaction();
 		// delete all existing positions
 		Query q = this.session.createQuery("delete from Position");
@@ -37,17 +28,115 @@ public class ReportTest {
 		// delete all existing accounts
 		q = this.session.createQuery("delete from Account");
 		q.executeUpdate();
+
+		// create default account with balance $5000
+		this.account = new Account(5000);
+		this.session.save(account);
 		tx.commit();
     }
 
+    @After
+    public void tearDown() {
+        this.session.close();
+        this.session = null;
+    }
+
 	@Test
-	public void test() throws Exception {
+	public void test1() throws Exception {
 		Order order = new BtOrder(this.session, this.account);
         order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 2);
         order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.20, 2);
         Report report = new Report(this.session, this.account);
         double totalPl = report.getProfitLoss();
-        System.out.println(totalPl);
+        assertEquals(totalPl, 200.0, 0.01);
+	}
+
+	@Test
+	public void test2() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 2);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.20, 1);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.30, 1);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 300.0, 0.01);
+	}
+
+	@Test
+	public void test3() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 1);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.20, 1);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.30, 2);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 300.0, 0.01);
+	}
+
+	@Test
+	public void test4() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 2);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.20, 3);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.30, 4);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.40, 1);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 800.0, 0.01);
+	}
+
+	@Test
+	public void test5() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketSell("EURUSD", "2015-09-07 00:00:00", 1.20, 2);
+        order.MarketBuy("EURUSD", "2015-09-08 00:00:00", 1.10, 2);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 200.0, 0.01);
+	}
+
+	@Test
+	public void test6() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.20, 1);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.30, 1);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 2);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 300.0, 0.01);
+	}
+
+	@Test
+	public void test7() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.30, 2);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 1);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.20, 1);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 300.0, 0.01);
+	}
+
+	@Test
+	public void test8() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.30, 4);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.40, 1);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 2);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.20, 3);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, 800.0, 0.01);
+	}
+
+	@Test
+	public void test9() throws Exception {
+		Order order = new BtOrder(this.session, this.account);
+        order.MarketBuy("EURUSD", "2015-09-07 00:00:00", 1.10, 2);
+        order.MarketSell("EURUSD", "2015-09-08 00:00:00", 1.00, 2);
+        Report report = new Report(this.session, this.account);
+        double totalPl = report.getProfitLoss();
+        assertEquals(totalPl, -200.0, 0.01);
 	}
 
 }
